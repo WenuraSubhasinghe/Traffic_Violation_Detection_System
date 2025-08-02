fd929f104d69fad25d51daa8521d52ecbf674fe9
Yazi715 <yasiruwr@gmail.com>
2025-08-02 12:02:20 +0530

    lane eval BE

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.routes_accidents import router as accidents_router
 from app.routes_speed import router as speed_router
 from app.routes_fraud import router as fraud_router
 from app.routes_lanechange import router as lane_router
+from app.routes_lanepatheval import router as lane_eval
 import os
 
 app = FastAPI(title="Traffic Backend")
@@ -34,6 +35,7 @@ app.include_router(accidents_router)
 app.include_router(speed_router)
 app.include_router(fraud_router)
 app.include_router(lane_router)
+app.include_router(lane_eval)
 
 
 @app.get("/")
